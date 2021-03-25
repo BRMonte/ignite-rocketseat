0364a5c0b393102a2797bcceb25a33d44f46ed8a
Bruno Montebello <brunormontebello@hotmail.com>
2021-03-25 08:45:07 +0000

    Update module

--- 01-github-explorer/dist/bundle.js
+++ 01-github-explorer/dist/bundle.js
@@ -1,9 +1,13 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var App = function App() {
-  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
-};
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/******/ })()
+;
